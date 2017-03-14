0162cf6cad1431717daba191298f255f2b5bd8d1
GWD <347757054@qq.com>
2017-03-14 09:11:45 +0800

    创建person类

---
+++ WeChat/WeChat/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  WeChat
+//
+//  Created by 古伟东 on 2017/3/14.
+//  Copyright © 2017年 victorgu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
